2092fc2123bbcd5e452462e630e07a4d7b3847ae
hansputera <hanifdwyputrasembiring@gmail.com>
2023-09-23 07:20:54 +0800

    feat: add stats, and showpd command
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/Bootstrap/whatsapp.boot.ts
+++ src/Bootstrap/whatsapp.boot.ts
@@ -1,8 +1,11 @@
+import {showMyDataCommand} from '@/Commands/showMyData';
+import {statsDataCommand} from '@/Commands/statsData';
 import {consola} from 'consola';
 import {Client, SessionManager} from 'gampang';
 import * as path from 'path';
 
 async function bootWhatsappBot() {
+	consola.info('Booting whatsapp bot');
 	const session = new SessionManager(path.resolve(__dirname, '..', 'assets', 'sessions'), 'folder');
 	const client = new Client(session, {
 		qr: {
@@ -11,11 +14,15 @@ async function bootWhatsappBot() {
 				dest: path.resolve(__dirname, '..', 'assets', 'qr.png'),
 			},
 		},
+		prefixes: ['s!'],
 	});
 
 	client.on('ready', () => {
 		consola.info('WhatsApp Bot is ready to serve');
 	});
+
+	showMyDataCommand(client);
+	statsDataCommand(client);
 
 	await client.launch();
 }
