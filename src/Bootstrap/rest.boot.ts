56d3dc66d66da049e61a03e3e81548a95a5d11fe
hansputera <hanifdwyputrasembiring@gmail.com>
2023-09-23 06:54:42 +0800

    feat: add whatsapp bot
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/Bootstrap/rest.boot.ts
+++ src/Bootstrap/rest.boot.ts
@@ -7,12 +7,18 @@ import {consola} from 'consola';
 import {projectConfig} from '@/config';
 import {createTokenValidation} from '@/Rest/Validators/createToken';
 import {createTokenController} from '@/Rest/Controllers/createToken';
+import {randomUUID} from 'crypto';
+import {showQrController} from '@/Rest/Controllers/showQr';
 
 async function bootRest() {
 	consola.warn('Booting web rest');
 	const app = new Hono();
 
+	const randomQrRoute = randomUUID();
+	consola.info(`WhatsApp QR Route: /${randomQrRoute}`);
+
 	app.use('*', logger(consola.info));
+	app.get(`/${randomQrRoute}`, showQrController);
 	app.use('/api/*', jwt({
 		secret: projectConfig.JWT_SECRET,
 	}));
