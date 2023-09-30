f8bd88106f77d655f618c30a27d37210c6bdbc25
hansputera <hanifdwyputrasembiring@gmail.com>
2023-09-30 17:06:47 +0800

    feat: add presensi
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

---
+++ prisma/migrations/20230930090627_patch_add_presensi/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `Presensi` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `rombel` VARCHAR(191) NOT NULL,
+    `idSiswa` VARCHAR(191) NOT NULL,
+    `tipe` ENUM('IZIN', 'SAKIT', 'ALPHA', 'HADIR') NOT NULL DEFAULT 'HADIR',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Presensi` ADD CONSTRAINT `Presensi_rombel_fkey` FOREIGN KEY (`rombel`) REFERENCES `Kelas`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Presensi` ADD CONSTRAINT `Presensi_idSiswa_fkey` FOREIGN KEY (`idSiswa`) REFERENCES `PesertaDidik`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
