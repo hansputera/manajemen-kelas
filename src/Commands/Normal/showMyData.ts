277e7787c1389f5aa478104f961e18a83325a848
hansputera <hanifdwyputrasembiring@gmail.com>
2023-09-30 17:09:57 +0800

    feat(showmydata): add option to show specified PesertaDidik
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/Commands/Normal/showMyData.ts
+++ src/Commands/Normal/showMyData.ts
@@ -1,15 +1,34 @@
 import {toZeroEightNumber} from '@/Utilities/toZeroEightNumber';
 import {prisma} from '@/prisma';
+import {RolePd} from '@prisma/client';
 import {type Client} from 'gampang';
 
 export const showMyDataCommand = (bot: Client) => {
 	bot.command('me', async ctx => {
 		const m = await ctx.reply('Mohon tunggu!');
-		const pd = await prisma.pesertaDidik.findFirst({where: {ponsel: toZeroEightNumber(ctx.authorNumber)}, include: {kelas: true}});
+		let pd = await prisma.pesertaDidik.findFirst({where: {ponsel: toZeroEightNumber(ctx.authorNumber)}, include: {kelas: true}});
 
 		if (!pd) {
 			await m?.edit('Ups, nomormu saat ini belum terdaftar dengan data periodik di Dapodik');
 			return;
+		}
+
+		if (pd.role === RolePd.ADMINISTRATOR) {
+			const asPd = ctx.getOption('asPd');
+			if (asPd.length) {
+				pd = await prisma.pesertaDidik.findFirst({
+					where: {
+						nisn: asPd.at(0),
+					},
+					include: {
+						kelas: true,
+					},
+				});
+				if (!pd) {
+					await ctx.reply('Tidak dapat menemukan peserta didik dengan NISN tersebut');
+					return;
+				}
+			}
 		}
 
 		await m?.edit(`Data terbaru pada ${pd.updatedAt.toLocaleDateString('id-ID', {
