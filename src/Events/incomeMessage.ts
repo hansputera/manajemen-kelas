6ed8a2b20e67b816fac65146140827592c75797a
hansputera <hanifdwyputrasembiring@gmail.com>
2023-10-01 12:11:28 +0800

    fix(laporPiket): fix regex matching flag
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/Events/incomeMessage.ts
+++ src/Events/incomeMessage.ts
@@ -3,7 +3,7 @@ import {handleLaporPiket} from './Executors/laporPiket';
 
 export const incomingMessage = (bot: Client) => {
 	bot.on('message', async ctx => {
-		const laporPiketRegex = /lapor piket(?:\s)?([a-zA-Z\s]+)?/gi;
+		const laporPiketRegex = /lapor piket(?:\s)?([a-zA-Z\s]+)?/i;
 
 		if (ctx.isPM && (laporPiketRegex.test(ctx.text) || ctx.image?.caption)) {
 			await handleLaporPiket(ctx, laporPiketRegex);
