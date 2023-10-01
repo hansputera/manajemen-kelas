5e093a95ce4aaeb5de736deb30ec7554efd55dac
hansputera <hanifdwyputrasembiring@gmail.com>
2023-10-01 11:22:52 +0800

    feat: add conclusion job sender
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/Bootstrap/index.ts
+++ src/Bootstrap/index.ts
@@ -1,3 +1,3 @@
 import './rest.boot';
-import './jobs.boot';
 import './whatsapp.boot';
+import './jobs.boot';
--- src/Bootstrap/jobs.boot.ts
+++ src/Bootstrap/jobs.boot.ts
@@ -1,9 +1,13 @@
+import {conclusionPiketJob} from '@/Jobs/conclusionPiketJob';
 import {syncPdJob} from '@/Jobs/syncPdJob';
 import {consola} from 'consola';
 
 async function bootJobs() {
 	consola.warn('Booting jobs');
 	await syncPdJob.trigger();
+
+	consola.info('SyncPdJob status: %s', syncPdJob.isRunning() ? 'running' : 'nonactive');
+	consola.info('ConclusionPiketJob status: %s', conclusionPiketJob.isRunning() ? 'running' : 'nonactive');
 }
 
 void bootJobs();
